69cad387dce524d792e1317c943eec31c02c058a
SymanziJ <97126299+SymanziJ@users.noreply.github.com>
2022-06-30 17:20:35 -0400

    spinner tests

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,5 +1,21 @@
 // Import the Spinner component into this file and test
 // that it renders what it should for the different props it can take.
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Spinner from './Spinner';
+
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).toBe(true)
 })
+
+test('Spinner renders if prop is true', () => {
+  render(<Spinner on={true}/>);
+  const spinnerText = screen.getByText(/please wait.../i);
+  expect(spinnerText).toBeInTheDocument;
+  })
+
+  test('Spinner does not renders if prop is false', () => {
+    render(<Spinner on={false}/>);
+    const spinnerText = screen.queryByText(/please wait.../i);
+    expect(spinnerText).not.toBeInTheDocument;
+    })
